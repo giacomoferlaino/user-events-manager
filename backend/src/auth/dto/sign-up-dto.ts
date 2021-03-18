d64aeeb2ab9d6880fe0ece3edb5b3720134a6438
Giacomo Ferlaino <giacomo.ferlaino@outlook.com>
2021-03-18 11:51:45 +0100

    feat: Added username validation during sign-up.

--- backend/src/auth/dto/sign-up-dto.ts
+++ backend/src/auth/dto/sign-up-dto.ts
@@ -1,3 +1,13 @@
 import { LoginDto } from './login-dto';
+import { IsNotEmpty, IsString } from 'class-validator';
 
-export class SignUpDto extends LoginDto {}
+export class SignUpDto extends LoginDto {
+  @IsNotEmpty()
+  @IsString()
+  username!: string;
+
+  populateFromObject(source: any) {
+    super.populateFromObject(source);
+    this.username = source['username'];
+  }
+}
