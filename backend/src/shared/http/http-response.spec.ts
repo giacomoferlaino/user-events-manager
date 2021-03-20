ea0134a13c040f1d9b9488540e72655a99b35416
Giacomo Ferlaino <giacomo.ferlaino@outlook.com>
2021-03-20 21:31:09 +0100

    feat: Added HttpResponse unit tests.

---
+++ backend/src/shared/http/http-response.spec.ts
@@ -0,0 +1,19 @@
+import { HttpResponse } from './http-response';
+
+describe('HttpResponse', () => {
+    let httpResponse: HttpResponse;
+
+    describe('toJSON', () => {
+        it('should return an empty array if not data is provided', () => {
+            httpResponse = new HttpResponse(undefined);
+            expect(httpResponse.toJSON().data).toStrictEqual([]);
+        });
+
+        it('should wrap the data inside an array if needed', () => {
+            const dataMock: any = {user: 'username'};
+            const expectedData = [dataMock];
+            httpResponse = new HttpResponse(dataMock);
+            expect(httpResponse.toJSON().data).toStrictEqual(expectedData);
+        });
+    });
+});
